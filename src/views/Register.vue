893cfbc302c98806a6bffa6940b904e9f1648f14
Shari Andr <al227832@edu.uaa.mx>
2020-06-05 10:18:09 -0500

    register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,10 +4,31 @@
              <b-form>
                  <label for="name">Nombre</label>
                  <b-input id="name" type="text" placeholder="Tu nombre" v-model="user.name"></b-input>
-                 <label for=""></label>
-                 <label for=""></label>
-                 <label for=""></label>
-                 <label for=""></label>
+                 <label for="lastName">Apellido</label>
+                 <b-input id="lastName" type="text" placeholder="Tu apellido" v-model="user.lastName"></b-input>
+                 <label for="email">Correo Electrónico</label>
+                 <b-input id="email" type="text" placeholder="Tu correo electrónico" v-model="user.email"></b-input>
+                 <label for="password">Contraseña</label>
+                 <b-input id="password" type="text" placeholder="Tu contraseña" v-model="user.password"></b-input>
+                 <label for="password">Confirmar contraseña</label> <!--xd-->
+                 <b-input id="password" type="text" placeholder="Tu contraseña" v-model="user.password"></b-input>                
+                 <label for="phone">Teléfono (opcional)</label>
+                 <b-input id="phone" type="text" placeholder="Tu teléfono" v-model="user.phone"></b-input> 
+                 <select v-model="user.location">
+                    <option disabled value="">Seleccione un país</option>
+                    <option value="México">México</option>
+                    <!--<option v-for="country in countries" v-bind:value="country">
+                        {{ country.name}} 
+                    </option>-->
+                </select> 
+                <select v-model="user.location">
+                    <option disabled value="">Seleccione un estado</option>
+                    <option value="Aguascalientes">Aguascalientes</option>
+                    <!--<option v-for="country in countries" v-bind:value="country">
+                        {{ country.name}} 
+                    </option>-->
+                </select>
+                
              </b-form>
     </div>
 </template>
