27f9fef0179fa68d8d99cc1ec9f8851292af97d2
Shari Andr <al227832@edu.uaa.mx>
2020-06-05 09:50:42 -0500

    register

---
+++ src/views/Register.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id=Register>
+        <h2>Regístrate</h2>
+             <b-form>
+                 <label for="name">Nombre</label>
+                 <b-input id="name" type="text" placeholder="Tu nombre" v-model="user.name"></b-input>
+                 <label for=""></label>
+                 <label for=""></label>
+                 <label for=""></label>
+                 <label for=""></label>
+             </b-form>
+    </div>
+</template>
+
+<script>
+export default {
+    //options null- luego aparecen los países
+}
+</script>
