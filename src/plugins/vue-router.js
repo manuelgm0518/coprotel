fa69354b4bb1703dde82a32a7df7d17863a0a0f3
unknown <manuel.gonzalez.0518@gmail.com>
2020-06-05 05:05:14 -0500

    Readded login

--- src/plugins/vue-router.js
+++ src/plugins/vue-router.js
@@ -11,6 +11,10 @@ export default new VueRouter({
       path: '/',
       name: 'Home',
       component: () => import('../views/Home.vue')
+    },{
+      path: '/LogIn',
+      name:'LogIn',
+      component: () => import('../views/LogIn.vue')
     }
   ]
 })
--- src/plugins/vuex-store.js
+++ src/plugins/vuex-store.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    serverPath:'http://localhost:3000'
   },
   mutations: {
   },
