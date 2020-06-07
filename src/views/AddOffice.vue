6873c58b8d21bdbf211384ddd6b150e1a2c001d4
JoakEmi <emiliano.pdz13@gmail.com>
2020-06-07 01:12:16 -0500

    Diseño AddOffice

--- src/views/AddOffice.vue
+++ src/views/AddOffice.vue
@@ -1,10 +1,241 @@
 <template>
     <div id="AddOffice" style="margin: 10px">
+
+        <b-card style="max-width: 60rem;" class=".card">
+
         <h1 style="margin-top: 100px;">Agregar una oficina</h1>
+
         <b-form> <!--Los inputs no están en orden-->
-            <b-input id="name" type="text" placeholder="Escribe un nombre de oficina" v-model="office.name.value" :state="office.name.state" @blur="office.name.validate(office.name)"></b-input>
+
+            <form ref="form" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.name.state"
+                    label="Name"
+                    label-for="name-input"
+                    invalid-feedback="Name is required"
+                >
+                <b-form-input
+                    id="name-input" 
+                    type="text" 
+                    v-model="office.name.value" 
+                    :state="office.name.state" 
+                    @blur="office.name.validate(office.name)"
+                    required
+                ></b-form-input>
+                </b-form-group>
+
+            </form>
+
+            <form ref="form1" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.description.state"
+                    label="Description"
+                    label-for="description-input"
+                    invalid-feedback="Description is required"
+                >
+                <AreaEditor
+                    id="description-input" 
+                    type="text" 
+                    v-model="office.description.value" 
+                    :state="office.description.state" 
+                    @update="updateDescription" 
+                    :content="office.description.value"
+                    required
+                />
+                </b-form-group>
+
+            </form>
+
+            <form ref="form2" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.state.state"
+                    label="State"
+                    label-for="state-input"
+                    invalid-feedback="State is required"
+                >
+
+                <b-form-select
+                    id="state-input"
+                    v-model="office.state.value" 
+                    :state="office.state.state"
+                    @change="changeMunicipalities(); office.state.validate(office.state)" 
+                    required
+                >
+                    <option 
+                        v-for="(state, k) in states" 
+                        v-bind:key="'k' + k" 
+                        :value="state._id">
+                            {{state.name}}  
+                    </option>
+                </b-form-select>
+
+                </b-form-group>
+
+            </form>
+
+            <form ref="form3" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.state.state"
+                    label="Location"
+                    label-for="location-input"
+                    invalid-feedback="Location is required"
+                >
+
+                <b-form-select
+                    id="location-input"
+                    v-model="office.location.value" 
+                    :state="office.location.state"
+                    @change="office.location.validate(office.location)" 
+                    required
+                > 
+                    <option 
+                        v-for="(municipality, l) in municipalities" 
+                        v-bind:key="'l' + l" 
+                        :value="municipality._id">
+                            {{municipality.name}}
+                    </option>
+                </b-form-select>
+
+                </b-form-group>
+
+            </form>
+
+            <form ref="form4" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.rentAmount.state"
+                    label="Rent Amount"
+                    label-for="rentAmount-input"
+                    invalid-feedback="Rent Amount is required"
+                >
+                <b-form-input
+                    id="rentAmount-input" 
+                    type="text" 
+                    v-model="office.rentAmount.value" 
+                    :state="office.rentAmount.state" 
+                    @blur="office.rentAmount.validate(office.rentAmount)"
+                    required
+                ></b-form-input>
+                </b-form-group>
+
+            </form>
+
+            <form ref="form5" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.contact.state"
+                    label="Contact"
+                    label-for="contact-input"
+                    invalid-feedback="Contact is required"
+                >
+                <AreaEditor
+                    id="contact-input" 
+                    type="text" 
+                    v-model="office.contact.value" 
+                    :state="office.contact.state" 
+                    @update="updateContact"
+                    required
+                />
+                </b-form-group>
+
+            </form>
+
+            <form ref="form6" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.area.state"
+                    label="Area"
+                    label-for="area-input"
+                    invalid-feedback="Area is required"
+                >
+                <b-form-input
+                    id="area-input" 
+                    type="text" 
+                    v-model="office.area.value" 
+                    :state="office.area.state" 
+                    @blur="office.area.validate(office.area)"
+                    required
+                ></b-form-input>
+                </b-form-group>
+
+            </form>
+
+            <form ref="form7" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.capacity.state"
+                    label="Capacity"
+                    label-for="capacity-input"
+                    invalid-feedback="Capacity is required"
+                >
+                <b-form-input
+                    id="capacity-input" 
+                    type="text" 
+                    v-model="office.capacity.value" 
+                    :state="office.capacity.state" 
+                    @blur="office.capacity.validate(office.capacity)"
+                    required
+                ></b-form-input>
+                </b-form-group>
+
+            </form>
+            
+            <form ref="form8" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.keywords.state"
+                    label="Keyword"
+                    label-for="keyword-input"
+                    invalid-feedback="Keyword is required"
+                >
+                <b-form-input
+                    id="keyword-input" 
+                    type="text" 
+                    v-for="(word, i) in office.keywords.value"
+                    v-bind:key="'i' + i"
+                    v-model="office.keywords.value[i]" 
+                    :state="office.keywords.state" 
+                    @input="addKeyword"
+                    @blur="office.keywords.validate(office.keywords);"
+                    required
+                ></b-form-input>
+                </b-form-group>
+
+            </form>
+
+            <form ref="form9" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group
+                    :state="office.images.state"
+                    label="Image"
+                    label-for="image-input"
+                    invalid-feedback="Image is required"
+                >
+                <!--<b-overlay :show="showOverlay" rounded="sm">-->
+                <b-file
+                    id="image-input"
+                    class="image"
+                    placeholder="Elije una imagen"
+                    v-for="(imag, j) in office.images.value"
+                    v-bind:key="'j' + j"
+                    v-model="office.images.value[j]"
+                    :state="office.images.state"
+                    @input="addImage(); office.images.validate(office.images);"
+                    required
+                ></b-file>
+                <!--</b-overlay>-->
+
+                </b-form-group>
+
+            </form>
+
+            <!--<b-form-input id="name" type="text" placeholder="Escribe un nombre de oficina" v-model="office.name.value" :state="office.name.state" @blur="office.name.validate(office.name)"></b-form-input>
             <AreaEditor @update="updateDescription" :content="office.description.value"/>
-            <p v-if="!office.description.state && office.description.state != null"><i style="color:red;">Debe escribir una descripción</i></p> <!--Falta ponerlo más bonito-->
+            <p v-if="!office.description.state && office.description.state != null"><i style="color:red;">Debe escribir una descripción</i></p> Falta ponerlo más bonito
             <b-select id="state" v-model="office.state.value" @change="changeMunicipalities(); office.state.validate(office.state)" :state="office.state.state">
                 <option v-for="(state, k) in states" v-bind:key="'k' + k" :value="state._id">{{state.name}}</option>
             </b-select>
@@ -13,7 +244,7 @@
             </b-select>
             <b-input id="rentAmount" type="text" placeholder="Escribe un precio de renta" v-model="office.rentAmount.value" :state="office.rentAmount.state" @blur="office.rentAmount.validate(office.rentAmount)"></b-input>
             <AreaEditor @update="updateContact" :content="office.contact.value"/>
-            <p v-if="!office.contact.state && office.contact.state != null"><i style="color:red;">Debe escribir los datos de contacto</i></p> <!--Falta ponerlo más bonito-->
+            <p v-if="!office.contact.state && office.contact.state != null"><i style="color:red;">Debe escribir los datos de contacto</i></p> Falta ponerlo más bonito
             <b-input id="area" type="text" placeholder="Escribe el área" v-model="office.area.value" :state="office.area.state" @blur="office.area.validate(office.area)"></b-input><p>Metros cuadrados</p>
             <b-input id="capacity" type="text" placeholder="Escribe la capacidad" v-model="office.capacity.value" :state="office.capacity.state" @blur="office.capacity.validate(office.capacity)"></b-input>
             <b-input
@@ -35,9 +266,10 @@
                 v-model="office.images.value[j]"
                 @input="addImage(); office.images.validate(office.images);"
                 :state="office.images.state"
-            ></b-file>
-            <b-button type="button" variant="warning" @click="add">Agregar</b-button>
+            ></b-file>-->
+            <b-button class="btn btn-primary btn-lg" style="float:right;" type="button" variant="warning" @click="add">Agregar</b-button>
         </b-form>
+        </b-card>
     </div>
 </template>
 
@@ -51,18 +283,19 @@ export default {
         return {
             office:{
                 name:{value:'', state:null, validate:this.validateText},
-                description:{value:'<p>Escribe una descripción de la oficina</p>', state:null, validate:this.validateTextArea},
+                description:{value:'<p></p>', state:null, validate:this.validateTextArea},
                 state:{value:'', state:null, validate:this.validateSelect},
                 location:{value:'', state:null, validate:this.validateSelect},
                 rentAmount:{value:'', state:null, validate:this.validateFloat},
-                contact:{value:'<p>Escribe los datos de contacto</p>', state:null, validate:this.validateTextArea},
+                contact:{value:'<p></p>', state:null, validate:this.validateTextArea},
                 area:{value:'', state:null, validate:this.validateFloat},
                 capacity:{value:'', state:null, validate:this.validateInt},
                 images:{value:[null], state:null, validate:this.validateMultImages},
                 keywords:{value:[''], state:null, validate:this.validateMultText}
             },
             states:[],
-            municipalities:[{_id:'', name:'Elija un municipio'}]
+            municipalities:[{_id:'', name:'Elija un municipio'}],
+            //showOverlay = false
         }
     },
     components:{
@@ -195,25 +428,36 @@ export default {
                 axios.post(this.$store.state.serverPath + '/api/office', send).then(async res => {
                     if(res.status == 200){
                         //////////////////////////////// Aquí estaría chido poner un overlay porque las imagenes tardan en subirse xd
+                        //this.showOverlay = true;
                         for(var img of this.office.images.value){
                             var formData = new FormData();
                             formData.append('image', img);
                             res = await axios.post(this.$store.state.serverPath + '/api/office/image/' + res.data._id, formData, { headers: {'Content-Type': 'multipart/form-data'}})
                         }
-                        alert('Aquí falta asignar la página de regreso'); //Falta poner la página de regreso
-                        this.$router.push('/');
+                        //this.showOverlay = false;
+                        this.$router.push('/profile');
                     }
                 }).catch(err => {
                     console.log(err);
                 });
             }
-            else
-                alert('Joaquín c la come porque no llenaste todos los campos'); //Falta ponerlo más bonito
+            else{
+                this.$bvToast.toast('Rellena todos los campos', {
+                            title: `Error`,
+                            variant: `danger`,
+                            toaster: `b-toaster-top-center`,
+                            solid: true
+                        })
+            }
         }
     }
 }
 </script>
 
 <style scoped>
-
+.card {
+        margin: 0 auto; /* Added */
+        float: none; /* Added */
+        margin-bottom: 10px; /* Added */
+}
 </style>
